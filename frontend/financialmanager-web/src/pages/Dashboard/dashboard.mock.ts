42346a5bd7a79f65f93be258d989ffdafe2b98e2
Gabriel <gapima7@gmail.com>
2025-12-24 14:15:21 -0300

    up

--- frontend/financialmanager-web/src/pages/Dashboard/dashboard.mock.ts
+++
@@ -1,9 +0,0 @@
-import type { DashboardResumo } from "./dashboard.types";
-
-export const dashboardMock: DashboardResumo = {
-  itens: [
-    { pessoaId: 1, nome: "Gabriel", totalReceitas: 5200, totalDespesas: 3100 },
-    { pessoaId: 2, nome: "Gabriela", totalReceitas: 4300, totalDespesas: 2500 },
-    { pessoaId: 3, nome: "Noah", totalReceitas: 0, totalDespesas: 800 },
-  ],
-};
